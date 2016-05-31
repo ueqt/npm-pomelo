e3960a7e5dafeb7b6268c60b174f1054fae4dbef
ueqt <ueqtxu@gmail.com>
2016-05-31 11:05:45 +0800

    add typings

---
+++ lib/pomelo.d.ts
@@ -0,0 +1,44 @@
+declare module Pomelo {    
+    
+    /**
+     * application
+     */
+    class App {
+        set(key: string, value: string): void;
+        configure(env: string, type: string, callback: () => any): void;
+        route(key: string, func: (session: any, msg: any, app: any, cb: any) => any): void;
+        filter(func: any): void;
+        start(): void;
+    }   
+    
+    /**
+     * Connector
+     */
+    class Connector {
+        sioconnector: any;
+        hybridconnector: any;
+        udpconnector: any;
+        mqttconnector: any;
+    }    
+    
+    /**
+     * PushScheduler
+     */
+    class PushScheduler {
+        direct: any;
+        buffer: any;
+    }
+    
+    var version: any;
+    var events: any;
+    var components: any;
+    var filters: any;
+    var rpcFilters: any;
+    var connectors: Connector;
+    var pushSchedulers: PushScheduler;
+    
+    function createApp(): App;
+    function timeout(): any;
+}
+
+export = Pomelo;
